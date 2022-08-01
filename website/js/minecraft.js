1cf1a6225df1acd04d6f13b08795b25cfeaba0c2
Ziga <ziga.zajc007@gmail.com>
2022-08-01 09:53:03 +0200

    Changed design and updated Tailwind

--- website/js/minecraft.js
+++ website/js/minecraft.js
@@ -23,11 +23,11 @@ if(geth[0] == 'server' && isPositiveInteger(geth[1]) && isIDValid(servers, geth[
 
 		data += "<tr>";
 		data += "<td class='px-4 py-4 whitespace-nowrap'><span class='inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-green-100 text-green-800'>" + (i + 1) + "</span></td>";
-		data += "<td class='px-4 py-4 whitespace-nowrap'><a href='minecraft.html?server=" + servers[i].id + "'>" + servers[i].name + "</a></td>";
+		data += "<td class='tertiaryColor px-4 py-4 whitespace-nowrap'><a href='minecraft.html?server=" + servers[i].id + "'>" + servers[i].name + "</a></td>";
 		data += "<td class='px-4 py-4 whitespace-nowrap text-sm text-gray-500' style='text-align: center;'><img class='rounded-t-md' style='min-width: 200px; max-height: 70px;' src='" + banner + "' onerror=\"this.src='images/nobanner.png'\" /><span class='inline-flex items-center px-2.5 py-0.5 text-sm rounded-b-md font-medium " + online_color + "' style='width: 100%;'><a href='#'>" + servers[i].ip + "</a></span></td>";
 		data += "<td class='px-4 py-4'><div>";
 		for(let j = 0; j < servers[i].categories.length; j++) data += "<span class='inline-flex items-center px-2 py-0.5 m-1 rounded text-xs font-medium bg-gray-100 text-gray-800'>" + servers[i].categories[j] + "</span>";
-		data += "</div></td><td class='px-4 py-4 whitespace-nowrap'>" + servers[i].players + " / " + servers[i].players_max + "</td>";
+		data += "</div></td><td class='tertiaryColor px-4 py-4 whitespace-nowrap'>" + servers[i].players + " / " + servers[i].players_max + "</td>";
 		data += "<td class='px-4 py-4 whitespace-nowrap'><span class='inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-blue-100 text-blue-800'>" + servers[i].version + "</span></td>";
 		data += "<td class='px-4 py-4 whitespace-nowrap'><span class='inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium " + online_color + "'>" + online + "</span></td>";
 		data += "</tr>";
