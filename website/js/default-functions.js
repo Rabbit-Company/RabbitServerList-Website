3edc807eabbeb4382cd9b788bdc3733615367336
Ziga <ziga.zajc007@gmail.com>
2023-03-21 14:36:12 +0100

    Update

--- website/js/default-functions.js
+++ website/js/default-functions.js
@@ -1,76 +1,41 @@
-var get = window.location.search.substr(1).split("&");
-var servers = ["minecraft"]; 
-
-for(let i = 0; i < servers.length; i++){
-  let jsonApp = localStorage.getItem(servers[i]);
-  let jsonTime = localStorage.getItem(servers[i] + "-time");
-
-  if(typeof(jsonApp) == 'undefined' || jsonApp == null){
-    fetchData(servers[i]);
-    continue;
-  }
-  if(typeof(jsonTime) == 'undefined' || jsonTime == null){
-  	fetchData(servers[i]);
-    continue;
-  }
-   
-  try{
-    JSON.parse(jsonApp);
-  }catch (e){
-    localStorage.setItem(servers[i], "{}");
-    localStorage.setItem(servers[i] + "-time", 0);
-    fetchData(servers[i]);
-    continue;
-  }
-
-  if(Number(jsonTime) + 3600000 < Date.now()){
-    fetchData(servers[i]);
-    continue;
-  }
+//Remove old data
+let keys = Object.keys(localStorage);
+for(let i = 0; i < keys.length; i++){
+	let time = localStorage.getItem(keys[i] + '-time');
+	if(time !== null && Number(time) + 1_800_000 < Date.now()){
+		localStorage.removeItem(keys[i]);
+		localStorage.removeItem(keys[i] + '-time');
+	}
 }
 
-function fetchData(type){
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "data/" + type + ".json");
+async function fetchServer(type, id){
+	await fetch("https://api.rabbitserverlist.com/v1/server/" + type + "/" + id).then((response) => response.json()).then((data) => {
+		localStorage.setItem('server-' + type + '-' + id, JSON.stringify(data.data));
+		localStorage.setItem('server-' + type + '-' + id + '-time', Date.now());
+	}).catch(() => {
+		localStorage.setItem('server-' + type + '-' + id, "{}");
+		localStorage.setItem('server-' + type + '-' + id + '-time', 0);
+	});
+}
 
-  xhr.setRequestHeader("Accept", "application/json");
-  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-  xhr.onreadystatechange = function () {
-      if(xhr.readyState === 4){
-          if(xhr.status != 200) return;
-          try {
-              JSON.parse(xhr.responseText);
-              localStorage.setItem(type, xhr.responseText);
-              localStorage.setItem(type + "-time", Date.now());
-              location.reload();
-          } catch (e) {
-              localStorage.setItem(type, "{}");
-              localStorage.setItem(type + "-time", 0);
-          }
-      }
-  };
-  xhr.send();
+async function fetchServers(type, page){
+	await fetch("https://api.rabbitserverlist.com/v1/servers/" + type + "/" + page).then((response) => response.json()).then((data) => {
+		localStorage.setItem('servers-' + type + '-' + page, JSON.stringify(data.data));
+		localStorage.setItem('servers-' + type + '-' + page + '-time', Date.now());
+	}).catch(() => {
+		localStorage.setItem('servers-' + type + '-' + page, "[]");
+		localStorage.setItem('servers-' + type + '-' + page + '-time', 0);
+	});
 }
 
 function isPositiveInteger(s) {
-  return /^\+?[1-9][\d]*$/.test(s);
-}
-
-function isIDValid(servers, id){
-  for(let i = 0; i < servers.length; i++) if(servers[i].id == id) return true;
-  return false;
-}
-
-function getServerData(servers, id){
-  for(let i = 0; i < servers.length; i++) if(servers[i].id == id) return servers[i];
-  return null;
+	return /^\+?[1-9][\d]*$/.test(s);
 }
 
 function toggleMenu(){
-  if(document.getElementById("mobile-menu").className == 'hidden pt-2 pb-3 space-y-1'){
-      document.getElementById("mobile-menu").className = 'pt-2 pb-3 space-y-1';
-  }else{
-      document.getElementById("mobile-menu").className = 'hidden pt-2 pb-3 space-y-1';
-  }
+	if(document.getElementById("mobile-menu").className == 'hidden pt-2 pb-3 space-y-1'){
+		document.getElementById("mobile-menu").className = 'pt-2 pb-3 space-y-1';
+	}else{
+		document.getElementById("mobile-menu").className = 'hidden pt-2 pb-3 space-y-1';
+	}
 }
--- website/js/minecraft.js
+++ website/js/minecraft.js
@@ -1,17 +1,24 @@
-let geth = get[0].split("=");
-var servers;
+const parms = new URLSearchParams(window.location.search);
 
-try{
-	servers = JSON.parse(localStorage.getItem("minecraft"))['servers'];
-}catch(err){
-	location.reload();
+let page = (parms.get('page') !== null && isPositiveInteger(parms.get('page'))) ? parms.get('page') : 1;
+if(page > 50) page = 50;
+
+let server = (parms.get('server') !== null && isPositiveInteger(parms.get('server'))) ? parms.get('server') : null;
+let serverData = localStorage.getItem('server-minecraft-' + server);
+
+if(server !== null && serverData === null){
+	await fetchServer('minecraft', server);
 }
 
-if(geth[0] == 'server' && isPositiveInteger(geth[1]) && isIDValid(servers, geth[1])){
+let servers = JSON.parse(localStorage.getItem('servers-minecraft-' + page));
+if(server === null && servers === null){
+	await fetchServers('minecraft', page);
+}
+
+if(server !== null){
 	document.getElementById('servers').className = "hidden";
 	document.getElementById('server').className = "";
-	let data = getServerData(servers, geth[1]);
-	document.getElementById("test").innerText = JSON.stringify(data);
+	document.getElementById("test").innerText = serverData;
 }else{
 	let table = document.getElementById("table_data");
 	let data = "";
@@ -20,13 +27,14 @@ if(geth[0] == 'server' && isPositiveInteger(geth[1]) && isIDValid(servers, geth[
 		let online = (servers[i].online) ? "Online" : "Offline";
 		let online_color = (servers[i].online) ? "bg-green-100 text-green-800" : "bg-red-100 text-red-800";
 		let banner = (servers[i].banner == null) ? 'images/nobanner.png' : servers[i].banner;
+		let categories = servers[i].categories.split(',');
 
 		data += "<tr>";
 		data += "<td class='px-4 py-4 whitespace-nowrap'><span class='inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-green-100 text-green-800'>" + (i + 1) + "</span></td>";
 		data += "<td class='tertiaryColor px-4 py-4 whitespace-nowrap'><a href='minecraft.html?server=" + servers[i].id + "'>" + servers[i].name + "</a></td>";
 		data += "<td class='px-4 py-4 whitespace-nowrap text-sm text-gray-500' style='text-align: center;'><img class='rounded-t-md' style='min-width: 200px; max-height: 70px;' src='" + banner + "' onerror=\"this.src='images/nobanner.png'\" /><span class='inline-flex items-center px-2.5 py-0.5 text-sm rounded-b-md font-medium " + online_color + "' style='width: 100%;'><a href='#'>" + servers[i].ip + "</a></span></td>";
 		data += "<td class='px-4 py-4'><div>";
-		for(let j = 0; j < servers[i].categories.length; j++) data += "<span class='inline-flex items-center px-2 py-0.5 m-1 rounded text-xs font-medium bg-gray-100 text-gray-800'>" + servers[i].categories[j] + "</span>";
+		for(let j = 0; j < categories.length; j++) data += "<span class='inline-flex items-center px-2 py-0.5 m-1 rounded text-xs font-medium bg-gray-100 text-gray-800'>" + categories[j] + "</span>";
 		data += "</div></td><td class='tertiaryColor px-4 py-4 whitespace-nowrap'>" + servers[i].players + " / " + servers[i].players_max + "</td>";
 		data += "<td class='px-4 py-4 whitespace-nowrap'><span class='inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-blue-100 text-blue-800'>" + servers[i].version + "</span></td>";
 		data += "<td class='px-4 py-4 whitespace-nowrap'><span class='inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium " + online_color + "'>" + online + "</span></td>";
@@ -37,5 +45,5 @@ if(geth[0] == 'server' && isPositiveInteger(geth[1]) && isIDValid(servers, geth[
 }
 
 document.getElementById("menu-toggle-btn").addEventListener('click', () => {
-  toggleMenu();
+	toggleMenu();
 });
