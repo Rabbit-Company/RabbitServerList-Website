69eb88d2502f83c42de4bf1c9980514ebb281b4f
Ziga <ziga.zajc007@gmail.com>
2023-03-21 17:02:11 +0100

    Update

--- website/js/minecraft.js
+++ website/js/minecraft.js
@@ -26,13 +26,12 @@ if(server !== null){
 	for(let i = 0; i < servers.length; i++){
 		let online = (servers[i].online) ? "Online" : "Offline";
 		let online_color = (servers[i].online) ? "bg-green-100 text-green-800" : "bg-red-100 text-red-800";
-		let banner = (servers[i].banner == null) ? 'images/nobanner.png' : servers[i].banner;
 		let categories = servers[i].categories.split(',');
 
 		data += "<tr>";
 		data += "<td class='px-4 py-4 whitespace-nowrap'><span class='inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-green-100 text-green-800'>" + (i + 1) + "</span></td>";
 		data += "<td class='tertiaryColor px-4 py-4 whitespace-nowrap'><a href='minecraft.html?server=" + servers[i].id + "'>" + servers[i].name + "</a></td>";
-		data += "<td class='px-4 py-4 whitespace-nowrap text-sm text-gray-500' style='text-align: center;'><img class='rounded-t-md' style='min-width: 200px; max-height: 70px;' src='" + banner + "' onerror=\"this.src='images/nobanner.png'\" /><span class='inline-flex items-center px-2.5 py-0.5 text-sm rounded-b-md font-medium " + online_color + "' style='width: 100%;'><a href='#'>" + servers[i].ip + "</a></span></td>";
+		data += "<td class='px-4 py-4 whitespace-nowrap text-sm text-gray-500' style='text-align: center;'><img class='rounded-t-md w-full' src='https://cdn.rabbitserverlist.com/servers/minecraft/banners/" + servers[i].id + "' onerror=\"this.src='images/nobanner.png'\" /><span class='inline-flex items-center px-2.5 py-0.5 text-sm rounded-b-md font-medium " + online_color + "' style='width: 100%;'><a href='#'>" + servers[i].ip + "</a></span></td>";
 		data += "<td class='px-4 py-4'><div>";
 		for(let j = 0; j < categories.length; j++) data += "<span class='inline-flex items-center px-2 py-0.5 m-1 rounded text-xs font-medium bg-gray-100 text-gray-800'>" + categories[j] + "</span>";
 		data += "</div></td><td class='tertiaryColor px-4 py-4 whitespace-nowrap'>" + servers[i].players + " / " + servers[i].players_max + "</td>";
