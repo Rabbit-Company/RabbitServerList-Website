3fc92405698446175fb5bb1c9ead9816d6cb4096
Ziga <ziga.zajc007@gmail.com>
2023-03-23 20:37:12 +0100

    Update

---
+++ website/js/panel.js
@@ -0,0 +1,13 @@
+import Utils from './utils.js';
+
+Utils.initialize();
+
+document.getElementById("menu-toggle-btn").addEventListener('click', () => {
+	Utils.toggleMenu();
+});
+
+try{
+	document.getElementById('logout').addEventListener('click', () => {
+		Utils.logout();
+	});
+}catch{}
