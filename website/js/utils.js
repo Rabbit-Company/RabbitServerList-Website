1f28c666ce6a45be63d8a3ce7d6719cb8d1996bb
Ziga <ziga.zajc007@gmail.com>
2023-03-30 16:35:03 +0200

    Update

--- website/js/utils.js
+++ website/js/utils.js
@@ -269,7 +269,7 @@ export default class Utils{
 			case 9:
 				//Already voted dialog
 				document.getElementById('dialog-icon').className = "mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10";
-				document.getElementById('dialog-icon').innerHTML = "<svg class='h-6 w-6 text-red-600' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'><path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z' /></svg>";
+				document.getElementById('dialog-icon').innerHTML = "<svg class='h-6 w-6 text-red-600' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'><path stroke='none' d='M0 0h24v24H0z' fill='none'></path><path d='M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0'></path><path d='M12 7v5l3 3'></path></svg>";
 
 				document.getElementById('dialog-title').innerText = "You can't vote yet!";
 				document.getElementById('dialog-text').innerText = text;
