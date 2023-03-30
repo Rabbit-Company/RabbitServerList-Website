c37f9443d4a81576e40c8f9d4dd1d47cee1039b1
Ziga <ziga.zajc007@gmail.com>
2023-03-30 12:13:09 +0200

    Better support for mobile

--- website/js/minecraft.js
+++ website/js/minecraft.js
@@ -308,7 +308,29 @@ function renderServers(servers){
 		data += "<tr>";
 		data += "<td class='px-2 py-4 whitespace-nowrap md:px-4 hidden md:table-cell'><span class='inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium " + online_color + "'>" + (i + 1) + "</span></td>";
 		data += "<td class='tertiaryColor px-2 py-4 whitespace-nowrap md:px-4 hidden xl:table-cell'><a href='?server=" + servers[i].id + "'>" + servers[i].name + "</a></td>";
-		data += "<td class='text-center px-2 py-4 whitespace-nowrap md:px-4 text-sm text-gray-500'><a href='?server=" + servers[i].id + "'><img class='rounded-t-md w-full' src='https://api.rabbitserverlist.com/v1/server/minecraft/" + servers[i].id + "/banner' /></a><span class='w-full inline-flex items-center px-2.5 py-0.5 text-sm rounded-b-md font-medium " + online_color + "'><a class='copyText cursor-pointer'>" + ip + "</a></span></td>";
+		data += `<td class='text-center px-2 py-4 whitespace-nowrap md:px-4 text-sm text-gray-500'>
+			<div class='hidden sm:block'>
+				<a href='?server=${servers[i].id}'>
+					<img class='rounded-t-md w-full' src='https://api.rabbitserverlist.com/v1/server/minecraft/${servers[i].id}/banner' />
+				</a>
+				<span class='w-full inline-flex items-center px-2.5 py-0.5 text-sm rounded-b-md font-medium ${online_color}'>
+					<a class='copyText cursor-pointer'>${ip}</a>
+				</span>
+			</div>
+			<div class='sm:hidden'>
+				<a href='?server=${servers[i].id}'>
+					<img class='rounded-md w-full' src='https://api.rabbitserverlist.com/v1/server/minecraft/${servers[i].id}/banner' />
+				</a>
+
+				<span class='mt-2 inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium ${online_color}'>${online}</span>
+				<a href='?version=${servers[i].version}'><span class='inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium blueBadge'>${servers[i].version}</span></a>
+				<span class='inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium blueBadge'>${servers[i].players} / ${servers[i].players_max}</span>
+				<br>
+				<span class='mt-2 w-full inline-flex items-center px-2.5 py-0.5 text-sm rounded-md font-medium ${online_color}'>
+					<a class='copyText cursor-pointer'>${ip}</a>
+				</span>
+			</div>
+		</td>`;
 		data += "<td class='px-2 py-4 md:px-4 hidden lg:table-cell'><div>";
 		for(let j = 0; j < categories.length; j++) data += "<a href='?category=" + categories[j] + "'><span class='inline-flex items-center px-2 py-0.5 m-1 rounded text-xs font-medium grayBadge'>" + categories[j] + "</span></a>";
 		data += "</div></td><td class='tertiaryColor px-2 py-4 md:px-4 whitespace-nowrap hidden sm:table-cell'>" + servers[i].players + " / " + servers[i].players_max + "</td>";
